3296d5ebeb4f238df53cc31d2532415f573acd8d
Tatevik Kalanchyan <tatev.kalanchyan@gamil.com>
2022-11-24 21:16:14 +0300

    Zadacha19

--- Zadacha19/Program.cs
+++ Zadacha19/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, W");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 10000 && number < 100000) 
+if ((number / 10000 == number % 10) && (number / 1000) % 10 == (number % 100) / 10)
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
+else
+{
+    Console.WriteLine("error: Вы ввели не пятизначное число");
+}
