e0ddcdd96ba509c9fc6f5228d19f6e5b9668ef70
Tatevik Kalanchyan <tatev.kalanchyan@gamil.com>
2022-11-24 21:16:55 +0300

    Zadacha21

--- Zadacha23/Program.cs
+++ Zadacha23/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, W!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов");
+
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine($"{i} в кубе равно -> {Math.Pow(i, 3)}");
+}
+
+
+
+
